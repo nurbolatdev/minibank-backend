be6cbaae4efbf3e3ae71241020b9b6ed920f1e83
nurbolat <djumadilovnurik9@gmail.com>
2026-03-05 18:13:00 +0500

    creation of architecture

---
+++ src/main/java/com/example/minibankbackend/MinibankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinibankBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minibankbackend/MinibankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
