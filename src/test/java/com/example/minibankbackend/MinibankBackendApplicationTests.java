01b2db0ef37d5903d1201dd88c4523f446cd1783
nurbolat <djumadilovnurik9@gmail.com>
2026-04-27 16:19:00 +0500

    test: make context load test use h2

--- src/test/java/com/example/minibankbackend/MinibankBackendApplicationTests.java
+++ src/test/java/com/example/minibankbackend/MinibankBackendApplicationTests.java
@@ -3,7 +3,15 @@ package com.example.minibankbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:minibank-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.show-sql=false",
+        "app.security.jwt.secret=change_this_secret_key_to_long_random_string_123456789"
+})
 class MinibankBackendApplicationTests {
 
     @Test
